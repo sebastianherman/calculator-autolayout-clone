4d269744dfada18dc6307d2b4c3cef2fbda9e8e4
Sebastian Herman <sebastian.herman@live.com>
2020-09-21 17:30:59 +0300

    final commit

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,61 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Sebastian Herman on 4/23/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        
+        if let n = number {
+            if symbol == "+/-" {
+                return n * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return n / 100
+            } else if symbol == "=" {
+                return performTwoNumCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        
+        return nil
+    }
+    
+    func performTwoNumCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "*":
+                return n1 * n2
+            case "/":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases")
+            }
+            
+        }
+        
+        return nil
+    }
+    
+}
